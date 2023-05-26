8397b0ee41dc8e5fd58b611615dc4c14816f82ff
edofazza <fazzari.edoardo@gmail.com>
2023-05-26 16:02:55 +0200

    dataloaders

--- code/training/auxiliary.py
+++ code/training/auxiliary.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import random
+import torchvision
+from torch.utils import data
 
 
 def create_sets(original_folder_path):
@@ -37,3 +39,15 @@ def create_sets(original_folder_path):
 
         # Move the directory
         shutil.move(source_path, destination_path)
+
+
+def create_dataloaders(train_path, val_path, test_path, transform, batch_size=1024):
+    train_data = torchvision.datasets.ImageFolder(root=train_path, transform=transform)
+    val_data = torchvision.datasets.ImageFolder(root=val_path, transform=transform)
+    test_data = torchvision.datasets.ImageFolder(root=test_path, transform=transform)
+    train_loader = data.Dataloader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = data.Dataloader(val_data, batch_size=batch_size, shuffle=True)
+    test_loader = data.Dataloader(test_data, batch_size=batch_size, shuffle=True)
+    return train_loader, val_loader, test_loader
+
+
