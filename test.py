726b2c21393a42157c891b4e49508086e311010b
edofazza <fazzari.edoardo@gmail.com>
2023-06-06 08:49:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import os
+
 from code.training.train_model import train_model, test_model
 from code.utils.configuration import create_configuration_file, load_configuration_file
 
 if __name__ == '__main__':
+    os.environ['TORCH_HOME'] = './cache'
     create_configuration_file()
     cfg = load_configuration_file('config.yaml')
     train_model(cfg)
