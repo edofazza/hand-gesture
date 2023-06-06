4263ee662a3263f5372e24e337c9afee78505996
edofazza <fazzari.edoardo@gmail.com>
2023-06-06 16:06:02 +0200

    Update models.py

--- code/training/models.py
+++ code/training/models.py
@@ -114,7 +114,7 @@ def get_model(model_name: str, pretrained_weights, finetune_layer, pretrained_mo
     if model_name.startswith('resnet') or model_name.startswith('inception'):
         model.fc = nn.Linear(model.fc.in_features, num_classes)
     elif model_name.startswith('densenet') or model_name.startswith('vgg') or model_name.startswith('efficientnet'):
-        model.classifier = nn.Linear(model.fc.in_features, num_classes)
+        model.classifier = nn.Linear(model.classifier.in_features, num_classes)
 
     if pretrained_weights:
         found_start_layer = False
