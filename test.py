62115378cec62a5dc4e0bfc6badbb50263e7e417
Edoardo Fazzari <46302551+edofazza@users.noreply.github.com>
2023-06-06 13:55:04 +0200

    tests

--- test.py
+++ test.py
@@ -5,9 +5,103 @@ from code.utils.configuration import create_configuration_file, load_configurati
 
 if __name__ == '__main__':
     os.environ['TORCH_HOME'] = './cache'
-    #create_configuration_file()
+    # RESNET 50
+    create_configuration_file()
     cfg = load_configuration_file('config.yaml')
-    #train_model(cfg)
+    train_model(cfg)
     cfg['pretrained_weights'] = False
     cfg['pretrained_model'] = True
     test_model(cfg)
+
+    #
+    create_configuration_file(model_name='resnet-fc-18')
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='resnet-fc-101')
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='densenet-fc-121')
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='vgg-fc-16')
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='inception_fc_v3')
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='efficientnet_fc_b0')
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='resnet-scratch-50', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='resnet-scratch-18', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='resnet-scratch-101', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='densenet-scratch-121', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='vgg-scratch-16', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='inception_scratch_v3', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+
+    create_configuration_file(model_name='efficientnet_scratch_b0', pretrained_weights=False)
+    cfg = load_configuration_file('config.yaml')
+    train_model(cfg)
+    cfg['pretrained_weights'] = False
+    cfg['pretrained_model'] = True
+    test_model(cfg)
+    
