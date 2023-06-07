708b308b4e022f955c779555600042f637e2edd3
edofazza <fazzari.edoardo@gmail.com>
2023-06-07 10:12:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from code.utils.configuration import create_configuration_file, load_configurati
 if __name__ == '__main__':
     os.environ['TORCH_HOME'] = './cache'
 
-    """create_configuration_file(model_name='custom_densenet_3_3_3', pretrained_weights=False)
+    create_configuration_file(model_name='custom_densenet_3_3_3', pretrained_weights=False)
     cfg = load_configuration_file('config.yaml')
     train_model(cfg)
     cfg['pretrained_model'] = True
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cfg = load_configuration_file('config.yaml')
     train_model(cfg)
     cfg['pretrained_model'] = True
-    test_model(cfg)"""
+    test_model(cfg)
 
     create_configuration_file(model_name='custom_resnet_3_3_3_cifar', pretrained_weights=False, pretrain_CIFAR=True)
     cfg = load_configuration_file('config.yaml')
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     cfg['pretrained_model'] = True
     test_model(cfg)
 
-    """create_configuration_file(model_name='custom_densenet_3_3_3_cifar', pretrained_weights=False, pretrain_CIFAR=True)
+    create_configuration_file(model_name='custom_densenet_3_3_3_cifar', pretrained_weights=False, pretrain_CIFAR=True)
     cfg = load_configuration_file('config.yaml')
     train_model(cfg)
     cfg['pretrained_model'] = True
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     cfg = load_configuration_file('config.yaml')
     train_model(cfg)
     cfg['pretrained_model'] = True
-    test_model(cfg)"""
+    test_model(cfg)
     
